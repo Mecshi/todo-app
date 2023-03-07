0f325383f6d4b095cc85f50c9d25edc8f06ccebb
Mecshi <maxi.correab@gmail.com>
2023-03-07 11:49:46 -0300

    refactor: Created file for constants

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const menuNav = [
+  {
+    link: 'All',
+    id: 1
+  },
+  {
+    link: 'Completed',
+    id: 2
+  },
+  {
+    link: 'Incompleted',
+    id: 3
+  },
+  {
+    link: 'Important',
+    id: 4
+  }
+]
