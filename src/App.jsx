9c4b9c90df5078d2eac5f1cc333e677a02920e65
Mecshi <maxi.correab@gmail.com>
2023-03-08 10:56:25 -0300

    feat: Add new component for Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Form } from './components/Form'
 import { NavBar } from './components/NavBar'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Form />
     </div>
   )
 }
