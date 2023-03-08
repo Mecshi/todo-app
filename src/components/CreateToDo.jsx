c72ffa24c59277fded64309fd5ff21b14fce4873
Mecshi <maxi.correab@gmail.com>
2023-03-08 14:37:48 -0300

    Add rule

--- src/components/CreateToDo.jsx
+++ src/components/CreateToDo.jsx
@@ -4,10 +4,14 @@ import { Modal } from './Modal'
 export const CreateToDo = () => {
   // create state for open modal
   const [isOpen, setIsOpen] = useState(false)
+  // add function for close modal
 
   return (
     <>
-      <button onClick={() => setIsOpen(!isOpen)} className={`text-tahiti-100 rounded-full bg-tahiti-200 cursor-pointer left-0 bottom-0 fixed p-2 m-4 hover:animate-spin ${isOpen ? 'hidden' : ''}`}>
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className={`text-tahiti-100 rounded-full bg-tahiti-200 cursor-pointer left-0 bottom-0 fixed p-2 m-4 hover:animate-spin ${isOpen ? 'hidden' : ''}`}
+      >
         <RiAddLine size={50} />
       </button>
 
