82c14b923502415d446b56b2afa9433e215bdad5
Mecshi <maxi.correab@gmail.com>
2023-03-09 16:53:26 -0300

    feat: Add new component with icons svg

---
+++ src/assets/Icons.jsx
@@ -0,0 +1,56 @@
+export const AddIcon = () => (
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-plus"
+    width={50}
+    height={50}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="#fff"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M12 5v14M5 12h14" />
+  </svg>
+)
+
+export const MenuIcon = () => (
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="hover:scale-110 hover:duration-200 hover:animate-bounce"
+    width={32}
+    height={32}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="#fff"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M4 6h16M8 12h8M6 18h12" />
+  </svg>
+)
+export const ExitIcon = () => (
+
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className='text-tahiti-200 hover:scale-110 hover:duration-200 hover:text-tahiti-100 hover:animate-bounce'
+    width={32}
+    height={32}
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="#FFB43A"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M18 6 6 18M6 6l12 12" />
+  </svg>
+
+)
