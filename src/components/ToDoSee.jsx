7b9310a1d7963cdbf3f89c68c99dcb39b41d8fc8
Mecshi <maxi.correab@gmail.com>
2023-03-09 00:23:28 -0300

    feat: Add cut description in render

--- src/components/ToDoSee.jsx
+++ src/components/ToDoSee.jsx
@@ -1,5 +1,29 @@
-export const ToDoSee = () => {
+export const ToDoSee = ({ toDos }) => {
   return (
-    <h1>Hola mundo</h1>
+    <section className='flex flex-col gap-5 mx-8'>
+      {
+        toDos.map(({ id, title, desc, cutDesc }) => {
+          return (
+            <div
+              className='flex flex-col gap-5 bg-tahiti-400 rounded-lg p-4'
+              key={id}
+            >
+              <h2 className='text-tahiti-100 text-2xl'>{title}</h2>
+              <p className='text-tahiti-100'>
+                {
+                  desc.length <= cutDesc.length
+                    ? desc
+                    : (
+                        cutDesc.at(-1) === ','
+                          ? cutDesc.slice(0, cutDesc.length - 1) + ' ...'
+                          : cutDesc + ' ...'
+                      )
+                }
+              </p>
+            </div>
+          )
+        })
+      }
+    </section>
   )
 }
