f024d5949705bc6f32f72d7a583fea209cdc9b8b
Mecshi <maxi.correab@gmail.com>
2023-03-10 16:45:02 -0300

    feat: Add links for navigate for page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,5 @@
+import { NavLink } from 'react-router-dom'
+
 // ...
 
 import { menuNav } from '../utils/constants'
@@ -10,25 +12,32 @@ export const NavBar = ({ navBar, setNavBar }) => {
     <nav className='flex justify-between fixed items-center h-20 px-8 w-full'>
 
       <div className='cursor-pointer hover:scale-200 hover:line-through decoration-gray-500 hover:opacity-70'>
+        <NavLink to='/' >
+          <h1 className='text-4xl font-semibold ml-2 text-tahiti-100'>To
+            <span className='text-tahiti-200'>Do!</span>
 
-        <h1 className='text-4xl font-semibold ml-2 text-tahiti-100'>To
-          <span className='text-tahiti-200'>Do!</span>
+          </h1>
 
-        </h1>
+        </NavLink>
 
       </div>
 
       <ul className='hidden md:flex gap-4'>
 
         {
-          menuNav.map(({ link }) => {
+          menuNav.map(({ link, path }) => {
             return (
-              <li
-                className='cursor-pointer text-tahiti-100 text-xl font-medium hover:text-tahiti-200 hover:duration-200 hover:line-through'
+              <NavLink
                 key={link}
+                to={path}
               >
-                {link}
-              </li>
+                <li
+                  className='cursor-pointer text-tahiti-100 text-xl font-medium hover:text-tahiti-200 hover:duration-200 hover:line-through'
+                  key={link}
+                >
+                  {link}
+                </li>
+              </NavLink>
             )
           })
         }
@@ -54,14 +63,20 @@ export const NavBar = ({ navBar, setNavBar }) => {
         navBar &&
         <ul className='flex flex-col justify-center items-center absolute top-0 left-0 h-screen w-full bg-gradient-to-b from-tahiti-500 to-tahiti-400'>
           {
-            menuNav.map(({ link, id }) => {
+            menuNav.map(({ link, id, path }) => {
               return (
-                <li
-                  className='px-4 cursor-pointer py-6 text-2xl font-medium hover:text-tahiti-200 text-tahiti-100 hover:scale-110 hover:duration-200 hover:line-through'
-                  key={id}
-                >
-                  {link}
-                </li>
+                <NavLink
+                key={link}
+                to={path}>
+
+                  <li
+                    className='px-4 cursor-pointer py-6 text-2xl font-medium hover:text-tahiti-200 text-tahiti-100 hover:scale-110 hover:duration-200 hover:line-through'
+                    key={id}
+                  >
+                    {link}
+                  </li>
+
+                </NavLink>
               )
             })
           }
