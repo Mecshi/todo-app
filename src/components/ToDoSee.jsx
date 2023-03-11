3c755dd23130f5598b7ee1360a03e51aa1403c4e
Mecshi <maxi.correab@gmail.com>
2023-03-11 18:54:45 -0300

    fix: Changed width for page rendering toDo

--- src/components/ToDoSee.jsx
+++ src/components/ToDoSee.jsx
@@ -6,37 +6,39 @@ import { ToDoCard } from './ToDoCard'
 export const ToDoSee = ({ toDos }) => {
   return (
 
-    <section
-      className='grid grid-cols-1 gap-5 mx-8 md:grid-cols-3 py-24'
-    >
-      {
-        examplesCard.map(({ title, desc }) => {
-          // create const for description rendering card
-          const cutDesc = desc.split(' ').slice(0, 24).join(' ')
-          return (
-            <ToDoCard
-              cutDesc = {cutDesc}
-              desc = {desc}
-              key = {title}
-              title = {title}
-            />
-          )
-        })
-      }
-      {
-        toDos.map(({ title, desc }) => {
-          // create const for description rendering card
-          const cutDesc = desc.split(' ').slice(0, 24).join(' ')
-          return (
-            <ToDoCard
-              cutDesc = {cutDesc}
-              desc = {desc}
-              key = {title}
-              title = {title}
-            />
-          )
-        })
-      }
+    <section className='flex justify-center '>
+      <div
+        className='grid grid-cols-1 gap-5 md:grid-cols-3 py-24 w-[70%]'
+      >
+        {
+          examplesCard.map(({ title, desc }) => {
+            // create const for description rendering card
+            const cutDesc = desc.split(' ').slice(0, 24).join(' ')
+            return (
+              <ToDoCard
+                cutDesc={cutDesc}
+                desc={desc}
+                key={title}
+                title={title}
+              />
+            )
+          })
+        }
+        {
+          toDos.map(({ title, desc }) => {
+            // create const for description rendering card
+            const cutDesc = desc.split(' ').slice(0, 24).join(' ')
+            return (
+              <ToDoCard
+                cutDesc={cutDesc}
+                desc={desc}
+                key={title}
+                title={title}
+              />
+            )
+          })
+        }
+      </div>
     </section>
   )
 }
