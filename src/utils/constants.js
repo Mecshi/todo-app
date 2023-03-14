6987912f310476d2dafbca1a1b17b6282d453acd
Mecshi <maxi.correab@gmail.com>
2023-03-13 21:43:57 -0300

    Commented examplesCard no used

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -21,30 +21,30 @@ export const menuNav = [
   }
 ]
 
-export const examplesCard = [
-  {
-    id: 0,
-    title: 'Add a ToDo',
-    desc: 'Click the "Add" button, enter a title (it is exclusive) and add a description, then press the "Save" button. And done!.'
-  },
-  {
-    id: 1,
-    title: 'Edit a ToDo',
-    desc: 'Click the three dots on the toDo card, select "Edit" and then make any changes you need!'
-  },
-  {
-    id: 2,
-    title: 'Delete a toDo',
-    desc: 'Click the three dots on the toDo card, select "Delete" and if you are sure, press Yes!'
-  },
-  {
-    id: 3,
-    title: 'Mark a toDo as Important',
-    desc: 'Press the star under the description and go to the menu, enter the Important section and you will see your saved toDo!'
-  },
-  {
-    id: 4,
-    title: 'Do you see three dots at the end of a toDo?',
-    desc: 'If you see the three dots, it means that the toDo has more content, to see it click on it, there you can see everything you have written!'
-  }
-]
+// export const examplesCard = [
+//   {
+//     id: 0,
+//     title: 'Add a ToDo',
+//     desc: 'Click the "Add" button, enter a title (it is exclusive) and add a description, then press the "Save" button. And done!.'
+//   },
+//   {
+//     id: 1,
+//     title: 'Edit a ToDo',
+//     desc: 'Click the three dots on the toDo card, select "Edit" and then make any changes you need!'
+//   },
+//   {
+//     id: 2,
+//     title: 'Delete a toDo',
+//     desc: 'Click the three dots on the toDo card, select "Delete" and if you are sure, press Yes!'
+//   },
+//   {
+//     id: 3,
+//     title: 'Mark a toDo as Important',
+//     desc: 'Press the star under the description and go to the menu, enter the Important section and you will see your saved toDo!'
+//   },
+//   {
+//     id: 4,
+//     title: 'Do you see three dots at the end of a toDo?',
+//     desc: 'If you see the three dots, it means that the toDo has more content, to see it click on it, there you can see everything you have written!'
+//   }
+// ]
