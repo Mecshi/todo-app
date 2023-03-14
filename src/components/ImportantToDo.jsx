5d87c90ba540b023eabd68ce6975406cf03d6116
Mecshi <maxi.correab@gmail.com>
2023-03-13 23:49:03 -0300

    feat: Created logic for Important page

---
+++ src/components/ImportantToDo.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux'
+import { ToDoCard } from './ToDoCard'
+export const ImportantToDo = () => {
+  const { toDoImportant } = useSelector(state => state)
+
+  return (
+    <section className='flex justify-center '>
+      <div
+        className='grid grid-cols-1 gap-5 md:grid-cols-3 py-24 w-[70%]'
+      >
+        {
+          toDoImportant.map(({ title, desc, cutDesc }) => {
+            return (
+              <ToDoCard
+                cutDesc={cutDesc}
+                desc={desc}
+                key={title}
+                title={title}
+              />
+            )
+          })
+        }
+      </div>
+    </section>
+  )
+}
