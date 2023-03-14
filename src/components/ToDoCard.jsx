914c5de8957ba4944bf01c1a3ce0f6e5f527cc89
Mecshi <maxi.correab@gmail.com>
2023-03-13 21:45:47 -0300

    feat:  Add important ToDo and used redux

--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -1,5 +1,7 @@
 // import { IconCard } from '../assets/Icons'
-
+import { addImportant, deleteImportant } from '../redux/actions'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 // ...
 
 export const ToDoCard = (
@@ -11,6 +13,34 @@ export const ToDoCard = (
   }
 
 ) => {
+  // config dispatch
+  const dispatch = useDispatch()
+
+  const { toDoImportant } = useSelector(state => state)
+
+  const [isImportant, setIsImportant] = useState(false)
+
+  const handleImportant = () => {
+    if (isImportant) {
+      setIsImportant(false)
+      dispatch(deleteImportant(title))
+    } else {
+      setIsImportant(true)
+      dispatch(addImportant({
+        title,
+        desc,
+        cutDesc
+      }))
+    }
+  }
+  useEffect(() => {
+    toDoImportant.forEach((toDo) => {
+      if (toDo.title === title) {
+        setIsImportant(true)
+      }
+    })
+  }, [toDoImportant])
+
   return (
 
     <div
@@ -25,7 +55,7 @@ export const ToDoCard = (
       </div>
 
       <section>
-        <p className='text-tahiti-100 mr-2 pt-4'>
+        <p className='text-tahiti-100 mr-2 pt-4 pb-3'>
           {
             desc.split(' ').length <= cutDesc.split(' ').length
               ? desc
@@ -38,6 +68,28 @@ export const ToDoCard = (
         </p>
 
         { /* Add setImportant here */ }
+
+        <div className='flex justify-end'>
+            {
+              isImportant
+                ? (
+                    <button
+                      className='p-1 rounded-sm hover:border hover:border-tahiti-200'
+                      onClick={handleImportant}
+                    >
+                      ❤️
+                    </button>
+                  )
+                : (
+                    <button
+                      className='p-1 rounded-sm hover:border hover:border-tahiti-200'
+                      onClick={handleImportant}
+                    >
+                      🤍
+                    </button>
+                  )
+            }
+        </div>
       </section>
     </div>
   )
